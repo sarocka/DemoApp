ff031fb2a8ad0121b5b8821a218c4c5a4894493f
petruska <sara.rat.ns.90@gmail.com>
2022-11-04 11:14:55 +0100

    Second Artboard created

---
+++ app/src/main/java/com/example/demoapp/ArtboardSecond.kt
@@ -0,0 +1,11 @@
+package com.example.demoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtboardSecond : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artboard_second)
+    }
+}
--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.demoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+     private lateinit var signInBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //hello
+        //btn_signIn
+        signInBtn=findViewById(R.id.btn_signIn)
+        signInBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, ArtboardSecond::class.java)
+            startActivity(intent)
+        }
 
     }
 }
